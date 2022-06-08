b47fc13e8bdd0281bd3377bd30586096c26fc888
Federico Tuñon Alves <tunonalves@gmail.com>
2022-06-07 22:27:30 -0300

    imagen blog

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from blog.models import Post, Categoria
 from .forms import formCat,formPost
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
 
 def blog(request):
-
     posts=Post.objects.all()
     return render(request, "blog/blog.html", {"posts": posts})
 
-def categoria(request, categoria_id):
 
+def categoria(request, categoria_id):
     categoria=Categoria.objects.get(id=categoria_id)
     posts=Post.objects.filter(categorias=categoria)
     return render(request, "blog/categoria.html", {'categoria':categoria,"posts": posts })
-
 
 
 def formpost(request):
@@ -24,13 +19,14 @@ def formpost(request):
         'formpost' : formPost()
     }
     if request.method == 'POST':
-        forms = formPost(data=request.POST)
+        forms = formPost(request.POST,request.FILES)
         if forms.is_valid():
             forms.save()
-        else:
-            data['formpost'] = forms
-    return render(request,"blog/post.html",data)
-
+            return redirect('Blog')
+    else:
+        #data['formpost'] = forms
+        forms = formPost()
+    return render(request,"blog/post.html",{'formpost' : forms})
 
 
 def formcat(request):
@@ -41,6 +37,7 @@ def formcat(request):
         forms = formCat(data=request.POST)
         if forms.is_valid():
             forms.save()
-        else:
-            data['formcat'] = forms
+            return redirect('Blog')
+    else:
+        data['formcat'] = forms
     return render(request,"blog/cats.html",data)
